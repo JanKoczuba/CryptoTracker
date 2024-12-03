704fb82babfd55a69a9bbd8ccecc9f5f227c215f
Jan Koczuba <schizobi@gmail.com>
2024-12-03 17:21:52 +0100

    added koin

--- app/src/main/java/com/koczuba/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/koczuba/cryptotracker/MainActivity.kt
@@ -7,11 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.koczuba.cryptotracker.crypto.presentation.coin_list.CoinListScreen
+import com.koczuba.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import com.koczuba.cryptotracker.ui.theme.CryptoTrackerTheme
+import org.koin.androidx.compose.koinViewModel
+import androidx.compose.runtime.getValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +25,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             CryptoTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    val viewModel = koinViewModel<CoinListViewModel>()
+                    val state by viewModel.state.collectAsStateWithLifecycle()
+                    CoinListScreen(
+                        state = state,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoTrackerTheme {
-        Greeting("Android")
     }
 }
