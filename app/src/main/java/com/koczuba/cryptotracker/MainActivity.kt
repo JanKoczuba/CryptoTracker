52f8f268b7d3e387bbbcfa81ac613b763932feb9
Jan Koczuba <schizobi@gmail.com>
2024-12-05 19:43:43 +0100

    changed navigation to NavigableListDetailPaneScaffold

--- app/src/main/java/com/koczuba/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/koczuba/cryptotracker/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.koczuba.cryptotracker
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,17 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.koczuba.cryptotracker.crypto.presentation.coin_list.CoinListScreen
-import com.koczuba.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import com.koczuba.cryptotracker.ui.theme.CryptoTrackerTheme
-import org.koin.androidx.compose.koinViewModel
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
-import com.koczuba.cryptotracker.core.presentation.util.ObserveAsEvents
-import com.koczuba.cryptotracker.core.presentation.util.toString
-import com.koczuba.cryptotracker.crypto.presentation.coin_details.CoinDetailsScreen
-import com.koczuba.cryptotracker.crypto.presentation.coin_list.CoinListEvent
+import com.koczuba.cryptotracker.core.navigation.AdaptiveCoinListDetailPane
 
 
 class MainActivity : ComponentActivity() {
@@ -29,36 +19,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CryptoTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val viewModel = koinViewModel<CoinListViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
-                    val context = LocalContext.current
-                    ObserveAsEvents(events = viewModel.events) { event ->
-                        when (event) {
-                            is CoinListEvent.Error -> {
-                                Toast.makeText(
-                                    context,
-                                    event.error.toString(context),
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }
-                    }
-                    when {
-                        state.selectedCoin != null -> {
-                            CoinDetailsScreen(
-                                state = state,
-                                modifier = Modifier.padding(innerPadding)
-                            )
-                        }
-
-                        else ->
-                            CoinListScreen(
-                                state = state,
-                                onAction = viewModel::onAction,
-                                modifier = Modifier.padding(innerPadding)
-                            )
-                    }
-
+                    AdaptiveCoinListDetailPane(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
